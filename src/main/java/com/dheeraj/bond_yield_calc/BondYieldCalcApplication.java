83fe3de495f639ff4ce07b2d19f825196f861693
Dheeraj <nizamdheerajkumar1@gmail.com>
2024-11-24 14:51:14 +0530

    initial project skeleton commit

---
+++ src/main/java/com/dheeraj/bond_yield_calc/BondYieldCalcApplication.java
@@ -0,0 +1,13 @@
+package com.dheeraj.bond_yield_calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BondYieldCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BondYieldCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dheeraj/bond_yield_calc/BondYieldCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dheeraj.bond_yield_calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BondYieldCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
